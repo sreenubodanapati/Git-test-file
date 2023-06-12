e3cec181233ed80d0848cdcbe0da09272f152c93
Sreenivasulu Bodanapati <sreenubodanapati>
2023-06-12 16:44:01 +0530

    added package.json file

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const { readFileSync, writeFileSync } = require("fs");
 const one = readFileSync("one.txt", "utf-8");
 
 const newfile = writeFileSync(
-  "onecopy.txt",
-  `This is copy on first file content: ${one}`,
-  { flag: "a" }
+    "onecopy.txt",
+    `This is copy on first file content: ${one}`,
+    { flag: "a" }
 );
+
+console.log("hello world");
